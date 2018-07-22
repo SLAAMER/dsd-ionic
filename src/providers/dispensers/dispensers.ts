e0803b33ac82955872063f7ab35322f4b3ae6201
Unknown <alexslash1997@gmail.com>
2018-07-22 02:02:20 -0700

    feat: can see generated dispenser uses in UI

--- src/providers/dispensers/dispensers.ts
+++ src/providers/dispensers/dispensers.ts
@@ -11,169 +11,169 @@ export class DispensersProvider {
   getDispensers(){
     return [{
       "dispenserId": "D1",
-      "priority": 20,
+      "probability": 20,
       "kits": [{
           "kitId": "K1",
-          "priority": 50
+          "probability": 50
         },
         {
           "kitId": "K2",
-          "priority": 75
+          "probability": 75
         },
         {
           "kitId": "K3",
-          "priority": 90
+          "probability": 90
         },
         {
           "kitId": "K4",
-          "priority": 100
+          "probability": 100
         }
       ],
       "users": [{
           "userId": "U1",
-          "priority": 10
+          "probability": 10
         },
         {
           "userId": "U2",
-          "priority": 25
+          "probability": 25
         },
         {
           "userId": "U3",
-          "priority": 69
+          "probability": 69
         },
         {
           "userId": "U4",
-          "priority": 99
+          "probability": 99
         },
         {
           "userId": "U5",
-          "priority": 100
+          "probability": 100
         }
       ]
     },
     {
       "dispenserId": "D2",
-      "priority": 50,
+      "probability": 50,
       "kits": [{
           "kitId": "K1",
-          "priority": 50
+          "probability": 50
         },
         {
           "kitId": "K2",
-          "priority": 75
+          "probability": 75
         },
         {
           "kitId": "K3",
-          "priority": 90
+          "probability": 90
         },
         {
           "kitId": "K4",
-          "priority": 100
+          "probability": 100
         }
       ],
       "users": [{
           "userId": "U1",
-          "priority": 10
+          "probability": 10
         },
         {
           "userId": "U2",
-          "priority": 25
+          "probability": 25
         },
         {
           "userId": "U3",
-          "priority": 80
+          "probability": 80
         },
         {
           "userId": "U4",
-          "priority": 99
+          "probability": 99
         },
         {
           "userId": "U4",
-          "priority": 100
+          "probability": 100
         }
       ]
     },
     {
       "dispenserId": "D3",
-      "priority": 60,
+      "probability": 60,
       "kits": [{
           "kitId": "K1",
-          "priority": 50
+          "probability": 50
         },
         {
           "kitId": "K2",
-          "priority": 75
+          "probability": 75
         },
         {
           "kitId": "K3",
-          "priority": 90
+          "probability": 90
         },
         {
           "kitId": "K4",
-          "priority": 100
+          "probability": 100
         }
       ],
       "users": [{
           "userId": "U1",
-          "priority": 10
+          "probability": 10
         },
         {
           "userId": "U2",
-          "priority": 25
+          "probability": 25
         },
         {
           "userId": "U3",
-          "priority": 80
+          "probability": 80
         },
         {
           "userId": "U4",
-          "priority": 99
+          "probability": 99
         },
         {
           "userId": "U4",
-          "priority": 100
+          "probability": 100
         }
       ]
     },
     {
       "dispenserId": "D4",
-      "priority": 100,
+      "probability": 100,
       "kits": [{
           "kitId": "K1",
-          "priority": 50
+          "probability": 50
         },
         {
           "kitId": "K2",
-          "priority": 75
+          "probability": 75
         },
         {
           "kitId": "K3",
-          "priority": 90
+          "probability": 90
         },
         {
           "kitId": "K4",
-          "priority": 100
+          "probability": 100
         }
       ],
       "users": [{
           "userId": "U1",
-          "priority": 10
+          "probability": 10
         },
         {
           "userId": "U2",
-          "priority": 25
+          "probability": 25
         },
         {
           "userId": "U3",
-          "priority": 80
+          "probability": 80
         },
         {
           "userId": "U4",
-          "priority": 99
+          "probability": 99
         },
         {
           "userId": "U4",
-          "priority": 100
+          "probability": 100
         }
       ]
     }
